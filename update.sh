64a9349d98e6aabc9b9c4afb1b55b03a61fb874f
hfst.co <39383536+hfstco@users.noreply.github.com>
2024-05-19 03:17:41 +0200

    init

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt install git, jq
+
+jq -c '.repos[]' repos.json | while read r; do
+        url=$(jq -r '.url' <<< $r);
+        repo=${url#*github.com/}
+        #user=${repo#*/}
+        #name=${repo%/*}
+
+        echo "Updating $url...";
+
+        jq -r '.branches[]' <<< $r | while read b; do
+                mkdir -p $repo/$b
+                cd $repo/$b
+                echo "$PWD"
+                git clone --recursive $url .
+                git checkout $b
+                git pull . $b
+
+                #compile
+                jq -r '.compile[]' <<< $r | while read c; do
+                        $c
+                done
+
+                cd ../../..
+                echo "$PWD"
+        done
+done
